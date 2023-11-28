be28f997b5c6f45751159f2357f940c1040fceb9
choikhwan <chokis426@gmail.com>
2023-11-28 11:29:40 +0900

    db 커밋

---
+++ epas-web/src/main/resources/static/db/USERS.sql
@@ -0,0 +1,14 @@
+﻿SET DEFINE OFF;
+Insert into DEVUSER.USERS
+   (ID, EMAIL, FIRST_NAME, LAST_NAME)
+ Values
+   (1, 'honggd@kcc.com', 'gilldong', 'hong');
+Insert into DEVUSER.USERS
+   (ID, EMAIL, FIRST_NAME, LAST_NAME)
+ Values
+   (2, 'choimw@kcc.com', 'minwoo', 'choi');
+Insert into DEVUSER.USERS
+   (ID, EMAIL, FIRST_NAME, LAST_NAME)
+ Values
+   (3, 'goldbell@kcc.com', 'goldbell', 'yoo2');
+COMMIT;
